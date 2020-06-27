4d55e9f536957077f8cdd3244ee196707a334135
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-06-27 03:30:53 +0100

    event_handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,12 +2,24 @@ import React from 'react'
 
 
 class Form extends React.Component {
-     
+    
+    /**
+     * use constructor if not using arrow functions on events
+     * 
+     */
+    //  constructor(props){
+    //      super(props);
+    //      this.handleSubmit=this.handleSubmit.bind(this);
+    //  }
     state={
       name : '',
-     quantity:0
+      quantity:0
     }; //donné saisi par l user
     
+    handleSubmit = (event) => {
+
+         console.log(this.state);
+    };
 
     render(){
 
@@ -16,7 +28,7 @@ class Form extends React.Component {
     <div>
      <h3>Add Items to Buy</h3>
 
-     <form>
+     <form onSubmit={e => e.preventDefault()}>
       <input type="number" placeholder="Quantity" value={this.state.quantity}
 
        onChange={ (event) => this.setState({quantity:event.target.value})} />
@@ -30,7 +42,7 @@ class Form extends React.Component {
       }
       />
 
-      <button type="submit">ADD</button>
+      <button type="submit" onClick={this.handleSubmit}>ADD</button>
 
      </form>
 
