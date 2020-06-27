38727948a2520819a42ddad2d22176ffc11af5b8
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-06-27 02:13:13 +0100

    first

--- hints.md
+++ hints.md
@@ -1,1 +1,4 @@
 ## creer un composant enfant : 
+
+* first release tag : git tag -a v1.0 id_commit -m 'comment'
+
---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    render(){
+
+        return(
+            <diV>
+            <h3>Item lists</h3>
+            <div>Waiting for Build</div>
+            </diV>
+            
+        );
+    }
+}
+
+export default App;
